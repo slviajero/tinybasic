3531a61b3148a9ba52313f5bfe0fc187ed3b40ea
Stefan <sl001@icloud.com>
2023-09-12 16:04:48 +0200

    Added uint32_t for all platforms

--- Basic2/Posix/hardware.h
+++ Basic2/Posix/hardware.h
@@ -123,6 +123,7 @@ typedef unsigned short uint16_t;
 typedef signed short int16_t;
 typedef unsigned char byte;
 typedef unsigned long long uint64_t;
+typedef unsigned int uint32_t;
 
 /*
  * additional prototypes in an non Arduino world 
