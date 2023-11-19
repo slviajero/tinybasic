aeeddb2cd4ae3308514bf36ccd6d7b1e6ca755e2
Stefan <sl001@icloud.com>
2023-11-19 19:00:30 +0100

    Moved to Basic1

---
+++ Basic1/TinybasicArduino/wifisettings.h
@@ -0,0 +1,5 @@
+const char* ssid = "daheim-iot";
+const char* password = "xacobeo7";
+const char* mqtt_server = "test.mosquitto.org";
+const short mqtt_port = 1883;
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xE9, 0xE9, 0xE9};
