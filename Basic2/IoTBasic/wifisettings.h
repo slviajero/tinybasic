2ef490cd89b16a02e1fcef0e30987e67bf64eda7
Stefan Lenz <stefan.lenz@serverfabrik.de>
2024-01-27 19:52:21 +0100

    mqqt username and password included

--- Basic2/IoTBasic/wifisettings.h
+++ Basic2/IoTBasic/wifisettings.h
@@ -1,5 +1,7 @@
-const char* ssid = "MYSSID";
-const char* password = "MYASS";
+const char* ssid = "";
+const char* password = "";
 const char* mqtt_server = "test.mosquitto.org";
+const char* mqtt_user = "";
+const char* mqtt_passwd = "";
 const short mqtt_port = 1883;
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xE9, 0xE9, 0xE9};
