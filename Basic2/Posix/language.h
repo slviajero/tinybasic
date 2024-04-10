b30b29847bbe1d2ca5a8045f7d23239be5be8359
Stefan Lenz <sl001@icloud.com>
2024-04-10 20:53:24 +0200

    Switched off long names by default, further tests needed

--- Basic2/Posix/language.h
+++ Basic2/Posix/language.h
@@ -36,8 +36,8 @@
  * BASICTINYWITHFLOAT: a floating point tinybasic, if you have 32kB and need complex device drivers
  * BASICMINIMAL: minimal language, just Palo Alto plus Arduino I/O, works on 168 with 1kB RAM and 16kB flash
  */
-#undef  BASICFULL
-#define	BASICINTEGER
+#define  BASICFULL
+#undef	BASICINTEGER
 #undef	BASICSIMPLE
 #undef	BASICMINIMAL
 #undef	BASICSIMPLEWITHFLOAT
