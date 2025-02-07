7b5715e42523f74baed7aabfb27feca04032b2b9
Stefan <sl001@icloud.com>
2025-02-07 20:06:26 +0100

    New Hardware definitions for the TTGO VGA 1.4 board for a nice screen

--- Basic2/IoTBasic/hardware.h
+++ Basic2/IoTBasic/hardware.h
@@ -389,16 +389,37 @@
 
 /* 
  * VGA system with SD card, based on the TTGO VGA 1.4 
- * ESP32 
- * standalone by default, with MQTT
+ * ESP32 with the following features: 
+ *   - VGA Screen
+ *   - 48 kB
+ *   - SD card  
+ *   - but definitely no MQTT
+ * 
+ * Standalone by default
  */
 #if defined(TTGOVGA)
 #define ARDUINOEEPROM
 #define ARDUINOVGA
 #define ARDUINOSD
-/* #define ARDUINOMQTT */ /* currently broken */
 #define SDPIN   13
+#undef  ARDUINOMQTT
 #define STANDALONE
+/* this is a large screen with 48 kB memory, good fonts, should work for most situations */
+#define MEMSIZE 48000
+#define TTGOVGARESOLUTION VGA_640x400_70Hz
+/* a smaller screen, slightly odd fonts but graph and 60 kB, good screen 
+#define MEMSIZE 60000
+#define TTGOVGARESOLUTION VGA_512x384_60Hz 
+*/
+/* Other options that work:
+ * #define MEMSIZE 48000
+ * #define TTGOVGARESOLUTION VGA_640x480_73Hz
+ * 
+ * #define TTGOVGARESOLUTION VGA_640x384_60Hz 
+ * 
+ * #define TTGOVGARESOLUTION VGA_640x200_70HzRetro 
+ */
+
 #endif
 
 /*
